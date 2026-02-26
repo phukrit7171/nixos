605e1973efb6a5cb5edb04779b7623d0cd914787
Mr.Phukrit Kittinontana <phukrit7171@gmail.com>
2026-02-26 21:28:20 +0700

    chore: config git

--- home/phukrit7171/core/git.nix
+++ home/phukrit7171/core/git.nix
@@ -1,10 +1,21 @@
 {
   programs.git = {
     enable = true;
+    lfs.enable = true;
     settings = {
       user.name = "Mr.Phukrit Kittinontana";
       user.email = "phukrit7171@gmail.com";
       init.defaultBranch = "main";
+      push.autoSetupRemote = true;
     };
+
+    includes = [
+      {
+        condition = "gitdir:/home/phukrit7171/Development/bu8";
+        contents = {
+          user.email = "phukrit.k@chanwanich.digital";
+        };
+      }
+    ];
   };
 }
