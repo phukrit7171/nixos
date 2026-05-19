cb336d9d7f7c05659a814d7feb51c2c2e36eada5
Mr.Phukrit Kittinontana <phukrit7171@gmail.com>
2026-05-19 22:37:13 +0700

    feat: make container can flash espressif device

--- modules/hardware/embedded.nix
+++ modules/hardware/embedded.nix
@@ -2,7 +2,8 @@
 
 {
   services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTR{idVendor}=="303a", ATTR{idProduct}=="1001", MODE="0666", GROUP="plugdev"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="303a", ATTRS{idProduct}=="1001", MODE="0666", GROUP="plugdev"
+    KERNEL=="ttyACM[0-9]*|ttyUSB[0-9]*", SUBSYSTEMS=="usb", ATTRS{idVendor}=="303a", ATTRS{idProduct}=="1001", MODE="0666", GROUP="plugdev"
   '';
 
   users.groups.plugdev = { };
